d4b7acf2ea1ee696ca3cc6e873f931fdb16c67d8
luxiangzheng <793247123@qq.com>
2020-05-30 14:12:54 +0800

    Junitperf测试

---
+++ src/main/java/com/lxz/junitperf/HtmlReporterTest.java
@@ -0,0 +1,13 @@
+package com.lxz.junitperf;
+
+import com.github.houbb.junitperf.core.annotation.JunitPerfConfig;
+import com.github.houbb.junitperf.core.report.impl.HtmlReporter;
+
+public class HtmlReporterTest {
+
+    @JunitPerfConfig(duration = 1000,reporter = (HtmlReporter.class))
+    public void Test() throws InterruptedException {
+        Thread.sleep(100);
+        System.out.println("Junit5");
+    }
+}
